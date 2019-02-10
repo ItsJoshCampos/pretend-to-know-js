b6e45c7576be0621bea9def682f7c314959b73c3
jshcmps <josh.campos@icloud.com>
2019-02-10 00:42:31 -0700

    template string content updates

--- 1-es6-template-strings.js
+++ 1-es6-template-strings.js
@@ -2,8 +2,13 @@ let firstName = 'josh';
 let lastName = 'campos';
 
 let welcomeUser = (firstName, lastName) => {
-  console.log('Welcome ' + lastName + ', ' + firstName + ' to the team!');
-  console.log(`Welcome ${firstName} ${lastName} to the team!`);
+  console.log(
+    "Welcome '" + lastName + ', ' + firstName + "' to the team! \n Thanks!"
+  );
+  console.log(`Welcome '${firstName} ${lastName}' to the team!
+  Thanks!`);
 };
+
+//avoid using escape keys (new lines), multiline support, interpolation vs concatenation
 
 welcomeUser(firstName, lastName);
